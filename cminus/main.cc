ab5777897e9f284f9bc8d0010c47e7b1865bf220
PurpleStripedUnicorn <pstripedunicorn@gmail.com>
2022-12-02 21:04:22 +0100

    Changed main to open files

--- cminus/main.cc
+++ cminus/main.cc
@@ -1,10 +1,25 @@
 
+#include <fstream>
 #include <iostream>
+#include <string>
+#include <sstream>
 #include "debugger/debugger.h"
 #include "lexer/lexer.h"
 
-int main() {
-    std::string txt = "Hello World!";
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cerr << "No input filename given." << std::endl;
+        return 1;
+    }
+    std::string filename = argv[1];
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Could not open file." << std::endl;
+        return 1;
+    }
+    std::stringstream stream;
+    stream << file.rdbuf();
+    std::string txt = stream.str();
     Lexer lexer(txt);
     std::vector<Token> tokens;
     while (!lexer.atEnd())
