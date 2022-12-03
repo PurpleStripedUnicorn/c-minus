3fd4bb8b4748abcc8c1860f8ca8bda0ecf6508bf
PurpleStripedUnicorn <pstripedunicorn@gmail.com>
2022-12-03 17:32:43 +0100

    Added some functions to parse nodes

--- cminus/parsenode/parsenode.cc
+++ cminus/parsenode/parsenode.cc
@@ -13,3 +13,19 @@ ParseNode::~ParseNode() {
 NodeType ParseNode::getType() const {
     return type;
 }
+
+const std::vector<ParseNode *> &ParseNode::children() const {
+    return childNodes;
+}
+
+ParseNode *ParseNode::getLeftChild() const {
+    if (childNodes.size() < 1)
+        return nullptr;
+    return childNodes[0];
+}
+
+ParseNode *ParseNode::getRightChild() const {
+    if (childNodes.size() < 2)
+        return nullptr;
+    return childNodes[1];
+}
--- cminus/parsenode/parsenode.h
+++ cminus/parsenode/parsenode.h
@@ -3,6 +3,7 @@
 #define FILE_PARSENODE
 
 #include "util/loc.h"
+#include <vector>
 
 enum NodeType {
     NODE_ERR,
@@ -40,12 +41,34 @@ public:
      */
     NodeType getType() const;
 
+    /**
+     * Get the children of the parse node
+     * @return A constant reference to the child nodes
+     */
+    const std::vector<ParseNode *> &children() const;
+
+    /**
+     * Get the first child of the parse node
+     * @return The first child in the child list, or nullptr of there are no
+     * children
+     */
+    virtual ParseNode *getLeftChild() const;
+
+    /**
+     * Get the second child of the parse node
+     * @return The second child in the child list, or nullptr of there is no
+     * more than one child
+     */
+    virtual ParseNode *getRightChild() const;
+
 private:
 
     // Type of the parse node
     NodeType type;
     // Location of the parse node
     Loc loc;
+    // Children of the node
+    std::vector<ParseNode *> childNodes;
 
 };
 
