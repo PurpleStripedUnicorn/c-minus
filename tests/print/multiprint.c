02bdb8e4fd6f43fe49be13983510017a2469b98b
PurpleStripedUnicorn <pstripedunicorn@gmail.com>
2022-12-12 18:20:40 +0100

    Added more tests

---
+++ tests/print/multiprint.c
@@ -0,0 +1,10 @@
+
+int main() {
+    int x, y;
+    x = 4;
+    print(x);
+    {
+        y = 4;
+        print(y);
+    }
+}
--- tests/scopes/scope3.c
+++ tests/scopes/scope3.c
@@ -3,9 +3,7 @@ int main() {
     int x, y, z;
     x = 44231;
     y = -78493;
-    {
-        int w;
-        w = x * y;
-        print(w);
-    }
+    int w;
+    w = x * y;
+    print(w);
 }
