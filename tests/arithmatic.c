d89770d5f3112dbf91e5802b19aa9f4a4341e51d
PurpleStripedUnicorn <pstripedunicorn@gmail.com>
2022-12-12 18:23:13 +0100

    Fixed tests to not use unary minus

--- tests/arithmatic.c
+++ tests/arithmatic.c
@@ -1,8 +1,8 @@
 
 int main() {
     int oop, _w;
-    oop = -43;
-    w = 17;
+    oop = 0 - 43;
+    _w = 17;
     _w = oop * _w;
     _w = w * w + w;
     print(_w);
--- tests/scopes/scope3.c
+++ tests/scopes/scope3.c
@@ -2,7 +2,7 @@
 int main() {
     int x, y, z;
     x = 44231;
-    y = -78493;
+    y = 78493;
     int w;
     w = x * y;
     print(w);
